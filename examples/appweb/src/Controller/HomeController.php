98a5adfc18fca83271bb918a6482ebee27ffe192
vacho <vachisinio@gmail.com>
2026-02-03 12:40:53 -0400

    [examples][appweb] Example of a controller

---
+++ examples/appweb/src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'name' => 'Osvaldo'
+        ]);
+    }
+}
