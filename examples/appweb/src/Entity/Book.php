764bd6321e660886de70c8cec611a49f41192637
vacho <vachisinio@gmail.com>
2026-02-04 11:11:45 -0400

    [examples][appweb] Book entity

---
+++ examples/appweb/src/Entity/Book.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ISBN = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Author = null;
+
+    #[ORM\Column]
+    private ?int $Edition = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): static
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getISBN(): ?string
+    {
+        return $this->ISBN;
+    }
+
+    public function setISBN(string $ISBN): static
+    {
+        $this->ISBN = $ISBN;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->Author;
+    }
+
+    public function setAuthor(string $Author): static
+    {
+        $this->Author = $Author;
+
+        return $this;
+    }
+
+    public function getEdition(): ?int
+    {
+        return $this->Edition;
+    }
+
+    public function setEdition(int $Edition): static
+    {
+        $this->Edition = $Edition;
+
+        return $this;
+    }
+}
